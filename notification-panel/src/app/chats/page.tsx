df6b534cb6820fbc0220f22f6dbb140f7c35e2cf
Dimitar <dimitar@dutchcodingcompany.com>
2024-08-09 13:00:39 +0200

    Add new routes

--- notification-panel/src/app/chats/page.tsx
+++ notification-panel/src/app/chats/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function Chats() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Home
+      Chats
     </main>
   );
 }
