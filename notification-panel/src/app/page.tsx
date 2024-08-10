b3a1fb45c18328415a65a9a310c4540e9b700fa7
Dimitar <dimitar@dutchcodingcompany.com>
2024-08-10 09:05:08 +0200

    Add prisma fetch actual data

--- notification-panel/src/app/page.tsx
+++ notification-panel/src/app/page.tsx
@@ -4,53 +4,65 @@ import { Notification } from "./types";
 import { NotificationTypes } from "./constants";
 import { NotificationOverview } from "./components/notification-overview";
 import { NotificationForm } from "./components/notification-form";
+import prisma from './lib/prisma';
 
-// Fetch notifications here
-const notifications: Notification[] = [
-	{
-		id: "1",
-		isRead: false,
-		type: NotificationTypes.PLATFORM_UPDATE,
-		releaseNumber: "1.3",
-	},
-	{
-		id: "2",
-		isRead: false,
-		type: NotificationTypes.ACCESS_GRANTED,
-		username: "User 1",
-		avatarUrl:
-			"https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80",
-	},
-	{
-		id: "3",
-		isRead: true,
-		type: NotificationTypes.COMMENT_TAG,
-		username: "Ivan Georgiev",
-		avatarUrl:
-			"https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&w=128&h=128&dpr=2&q=80",
-	},
-	{
-		id: "4",
-		isRead: true,
-		type: NotificationTypes.JOIN_WORKSPACE,
-		username: "User with very long name",
-	},
-	{
-		id: "5",
-		isRead: false,
-		type: NotificationTypes.JOIN_WORKSPACE,
-		username: "User with very long name",
-	},
-	{
-		id: "6",
-		isRead: false,
-		type: NotificationTypes.JOIN_WORKSPACE,
-		username: "User with very long name",
-	},
-];
+// TODO: Delete later static notifications
+// const notifications: Notification[] = [
+// 	{
+// 		id: "1",
+// 		isRead: false,
+// 		type: NotificationTypes.PLATFORM_UPDATE,
+// 		releaseNumber: "1.3",
+// 	},
+// 	{
+// 		id: "2",
+// 		isRead: false,
+// 		type: NotificationTypes.ACCESS_GRANTED,
+// 		username: "User 1",
+// 		avatarUrl:
+// 			"https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80",
+// 	},
+// 	{
+// 		id: "3",
+// 		isRead: true,
+// 		type: NotificationTypes.COMMENT_TAG,
+// 		username: "Ivan Georgiev",
+// 		avatarUrl:
+// 			"https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&w=128&h=128&dpr=2&q=80",
+// 	},
+// 	{
+// 		id: "4",
+// 		isRead: true,
+// 		type: NotificationTypes.JOIN_WORKSPACE,
+// 		username: "User with very long name",
+// 	},
+// 	{
+// 		id: "5",
+// 		isRead: false,
+// 		type: NotificationTypes.JOIN_WORKSPACE,
+// 		username: "User with very long name",
+// 	},
+// 	{
+// 		id: "6",
+// 		isRead: false,
+// 		type: NotificationTypes.JOIN_WORKSPACE,
+// 		username: "User with very long name",
+// 	},
+// ];
 
-export default function Home() {
-	// TODO Extra: Move the router to a separate client side component so this one can be server side
+async function getNotifications(): Promise<Notification[] | []> {
+  try {
+    const notifications = await prisma.notification.findMany() as Notification[];
+
+    return notifications ? notifications : [];
+  } catch (error) {
+    console.error('Error fetching notifications:', error);
+    return [];
+  }
+}
+
+export default async function Home() {
+	const notifications = await getNotifications();
 
 	return (
 		<>
@@ -68,11 +80,15 @@ export default function Home() {
 
 				<Popover.Portal>
 					<Popover.Content>
-						<NotificationOverview
-							notifications={notifications}
-						/>
+						{notifications.length > 0 && (
+							<>
+								<NotificationOverview
+									notifications={notifications}
+								/>
+								<Popover.Arrow className="fill-white" />
+							</>)
+						}
 
-						<Popover.Arrow className="fill-white" />
 					</Popover.Content>
 				</Popover.Portal>
 			</Popover.Root>
