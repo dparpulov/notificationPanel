dcb079989bd7073b144185bedf171f1aa472778e
Dimitar <dimitar@dutchcodingcompany.com>
2024-09-08 10:10:56 +0300

    Update build script and add postinstall for prisma to fix deployment

--- notification-panel/src/app/lib/prisma.ts
+++ notification-panel/src/app/lib/prisma.ts
@@ -5,6 +5,7 @@ let prisma: PrismaClient;
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 } else {
+  // TODO make this a singleton if I use prisma in multiple files
   const globalWithPrisma = global as typeof globalThis & {
     prisma: PrismaClient;
   };
